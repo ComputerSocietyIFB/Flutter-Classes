01ac3534f5a717773a5a72013e8d35633f90ce36
s7thiago <thyagosousasilva@gmail.com>
2020-06-28 23:15:14 -0300

    Criando primeiro projeto de exemplo da aula 4

---
+++ aulas/aula-4/dart/dart_extension_functions_example/bin/dart_extension_functions_example.dart
@@ -0,0 +1,5 @@
+import 'package:dart_extension_functions_example/dart_extension_functions_example.dart' as dart_extension_functions_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_extension_functions_example.calculate()}!');
+}
---
+++ aulas/aula-4/dart/dart_extension_functions_example/test/dart_extension_functions_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_extension_functions_example/dart_extension_functions_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
